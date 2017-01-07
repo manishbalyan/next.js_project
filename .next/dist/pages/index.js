247f10546a80986b9a271d667538cd29987dc23b
manishbalyan <balyan05.manish@gmail.com>
2017-01-07 12:36:24 +0530

    first commit

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('/home/manish/Documents/Workspace/next.js_project/node_modules/react/react.js');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return _react2.default.createElement(
+    'div',
+    null,
+    'Hello world!'
+  );
+}; /**
+    * Created by manish on 7/1/17.
+    */
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUdBOzs7Ozs7a0JBRWU7QUFBQSxTQUFNO0FBQUE7QUFBQTtBQUFBO0FBQUEsR0FBTjtBQUFBLEMsRUFMZiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZVJvb3QiOiIvaG9tZS9tYW5pc2gvRG9jdW1lbnRzL1dvcmtzcGFjZS9uZXh0LmpzX3Byb2plY3QiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIENyZWF0ZWQgYnkgbWFuaXNoIG9uIDcvMS8xNy5cbiAqL1xuaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0J1xuXG5leHBvcnQgZGVmYXVsdCAoKSA9PiA8ZGl2PkhlbGxvIHdvcmxkITwvZGl2PiJdfQ==
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by manish on 7/1/17.
+ */
+import React from 'react'
+
+export default () => <div>Hello world!</div>
